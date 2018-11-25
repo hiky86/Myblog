3a8cbd8eccf6f2c2c668d227287330a25d44cb53
EC2 Default User <ec2-user@ip-172-31-62-173.ec2.internal>
2018-11-25 08:14:26 +0000

    Add contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181124131306) do
+ActiveRecord::Schema.define(version: 20181125080653) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
